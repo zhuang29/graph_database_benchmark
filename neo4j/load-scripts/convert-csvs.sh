0566b8019f702c8bf8ab28bf34ab5a6e8a220a57
Jun Hyung Shin <junhyung.shin@tigergraph.com>
2019-01-18 17:36:36 -0800

    Modify loading scripts
    To match with the same raw data file for TigerGraph

--- neo4j/load-scripts/convert-csvs.sh
+++ neo4j/load-scripts/convert-csvs.sh
@@ -1,28 +1,20 @@
 #!/bin/bash
 # . path.sh
 
-# remove the first line of each csv file
-files=$(find ${NEO4J_DATA_DIR} -name '*.csv')
-for csv in $files
-do
-  sed '1d' $csv > tmp.csv && mv tmp.csv $csv
-done
-
-# add header files
+# replace headers
 while read line; do
   IFS=' ' read -r -a array <<< $line
   filename=${array[0]}
   header=${array[1]}
-  touch ${NEO4J_DATA_DIR}/${filename}_header.csv
-  echo "${header}" > ${NEO4J_DATA_DIR}/${filename}_header.csv
+  sed -i "1s/.*/$header/" "${NEO4J_DATA_DIR}/${filename}${POSTFIX}"
 done < headers.txt
 
 # replace labels with one starting with an uppercase letter
-find ${NEO4J_DATA_DIR} -name 'place_[0-9]*.csv' -exec sed -i "s/|city$/|City/" {} \;
-find ${NEO4J_DATA_DIR} -name 'place_[0-9]*.csv' -exec sed -i "s/|country$/|Country/" {} \;
-find ${NEO4J_DATA_DIR} -name 'place_[0-9]*.csv' -exec sed -i "s/|continent$/|Continent/" {} \;
-find ${NEO4J_DATA_DIR} -name 'organisation_[0-9]*.csv' -exec sed -i "s/|company$/|Company/" {} \;
-find ${NEO4J_DATA_DIR} -name 'organisation_[0-9]*.csv' -exec sed -i "s/|university$/|University/" {} \;
+sed -i "s/|city$/|City/" "${NEO4J_DATA_DIR}/place${POSTFIX}"
+sed -i "s/|country$/|Country/" "${NEO4J_DATA_DIR}/place${POSTFIX}"
+sed -i "s/|continent$/|Continent/" "${NEO4J_DATA_DIR}/place${POSTFIX}"
+sed -i "s/|company|/|Company|/" "${NEO4J_DATA_DIR}/organisation${POSTFIX}"
+sed -i "s/|university|/|University|/" "${NEO4J_DATA_DIR}/organisation${POSTFIX}"
 
 # convert each date of format yyyy-mm-dd to a number of format yyyymmddd
 # find ${NEO4J_DATA_DIR} -name 'person_[0-9]*.csv' -exec sed -i "s#|\([0-9][0-9][0-9][0-9]\)-\([0-9][0-9]\)-\([0-9][0-9]\)|#|\1\2\3|#g" {} \;

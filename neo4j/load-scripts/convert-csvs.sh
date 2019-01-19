ed236b1202ff34dfe509b053b377f3f36d4322e8
Jun Hyung Shin <junhyung.shin@tigergraph.com>
2019-01-18 22:12:31 -0800

    Modify sed not to operate in place
    Performance boost when make tmp file and then mv.
    Fix typos.

--- neo4j/load-scripts/convert-csvs.sh
+++ neo4j/load-scripts/convert-csvs.sh
@@ -6,15 +6,15 @@ while read line; do
   IFS=' ' read -r -a array <<< $line
   filename=${array[0]}
   header=${array[1]}
-  sed -i "1s/.*/$header/" "${NEO4J_DATA_DIR}/${filename}${POSTFIX}"
+  sed "1s/.*/$header/" ${NEO4J_DATA_DIR}/${filename}${NEO4J_DATA_POSTFIX} > tmp.csv && mv tmp.csv ${NEO4J_DATA_DIR}/${filename}${NEO4J_DATA_POSTFIX}
 done < headers.txt
 
 # replace labels with one starting with an uppercase letter
-sed -i "s/|city$/|City/" "${NEO4J_DATA_DIR}/place${POSTFIX}"
-sed -i "s/|country$/|Country/" "${NEO4J_DATA_DIR}/place${POSTFIX}"
-sed -i "s/|continent$/|Continent/" "${NEO4J_DATA_DIR}/place${POSTFIX}"
-sed -i "s/|company|/|Company|/" "${NEO4J_DATA_DIR}/organisation${POSTFIX}"
-sed -i "s/|university|/|University|/" "${NEO4J_DATA_DIR}/organisation${POSTFIX}"
+sed -i "s/|city$/|City/" "${NEO4J_DATA_DIR}/place${NEO4J_DATA_POSTFIX}"
+sed -i "s/|country$/|Country/" "${NEO4J_DATA_DIR}/place${NEO4J_DATA_POSTFIX}"
+sed -i "s/|continent$/|Continent/" "${NEO4J_DATA_DIR}/place${NEO4J_DATA_POSTFIX}"
+sed -i "s/|company|/|Company|/" "${NEO4J_DATA_DIR}/organisation${NEO4J_DATA_POSTFIX}"
+sed -i "s/|university|/|University|/" "${NEO4J_DATA_DIR}/organisation${NEO4J_DATA_POSTFIX}"
 
 # convert each date of format yyyy-mm-dd to a number of format yyyymmddd
 # find ${NEO4J_DATA_DIR} -name 'person_[0-9]*.csv' -exec sed -i "s#|\([0-9][0-9][0-9][0-9]\)-\([0-9][0-9]\)-\([0-9][0-9]\)|#|\1\2\3|#g" {} \;
